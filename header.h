2b1c5e5002ef6118fcd79fad8a2ff6139d7708c1
Wibbly <bb232399@gmail.com>
2024-06-14 20:29:10 +0800

    match測試成功，exe已更新，output檔和map建立完畢

--- header.h
+++ header.h
@@ -5,9 +5,12 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <map>
+#include <sstream>
 
 using namespace std;
 
 #define ARGV_INPUT_INDEX 1
+#define ARGV_OUTPUT_INDEX 2
 
 #endif
